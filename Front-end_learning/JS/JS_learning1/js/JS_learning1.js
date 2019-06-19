69e42ba36796ccd0346a33f7e0d10d269acd0ce6
dcyljr <310547343@qq.com>
2019-06-19 09:20:59 +0800

    原生JS显示与隐藏内容

--- Front-end_learning/JS/JS_learning1/js/JS_learning1.js
+++ Front-end_learning/JS/JS_learning1/js/JS_learning1.js
@@ -2,6 +2,18 @@ window.onload = function () {
     var t_btn = document.getElementById('btn');
     t_btn.onclick = function () {
         var x = document.getElementsByClassName('text');
-        x[0].style.backgroundColor = "red";
+        x[0].style.color = "red";
+    }
+}
+
+window.onload = function () {
+    var wenzi = document.getElementById('text');
+    var div = document.getElementById('menu');
+    wenzi.onclick = function () {
+        if (div.style.display == 'block') { // == 判断div.display是否为显示
+            div.style.display = 'none'; //= 赋值也可了解成改变
+        } else {
+            div.style.display = 'block';
+        }
     }
 }
